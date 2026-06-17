f5e0ae773ebbaf2addfb85739e1bb71f35ebba1a
Amit Rao <amit.raao04@gmail.com>
2026-06-17 13:11:10 +0530

    Initial commit - 5 DocTypes for Stellar Brands

--- stellar_brands/hooks.py
+++ stellar_brands/hooks.py
@@ -247,3 +247,11 @@ app_license = "mit"
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+
+fixtures = [
+    "Scraping Source",
+    "Scrape Run",
+    "Product",
+    "Submission",
+    "Buyer"
+]

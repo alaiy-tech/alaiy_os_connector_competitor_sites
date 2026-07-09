e048f204ef22c07bd472f4fd68bc0edd684a65d2
Sarthak S Kumar <sarthakskumar7@gmail.com>
2026-07-09 18:27:04 +0530

    feat(competitor_sites_connector): add __init__, scrape_runner and website_manager implementation in Python (12 files)

--- competitor_sites_connector/alaiy_os_connector_competitor_sites/page/scrape_runner/scrape_runner.js
+++
@@ -1,9 +0,0 @@
-frappe.pages["scrape-runner"].on_page_load = function (wrapper) {
-	frappe.ui.make_app_page({ parent: wrapper, title: "Scrape Runner", single_column: true });
-	const body = $(wrapper).find(".layout-main-section");
-	body.addClass("sb-sr-page-body");
-	body.html(window._sb_sr_html());
-	const root = body.find(".sb-sr")[0];
-	window._sb_sr_load(root);
-	window._sb_sr_bind_events(root);
-};
